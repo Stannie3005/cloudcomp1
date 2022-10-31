5b3d6644600ec664f7cfa038dc61374fa48225cb
IITS\se19sdd <s196269@stud.uni-ruse.bg>
2022-10-31 11:14:02 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :) <br>
+        <a href="contacts.php">Контакти</a>
+    </body>
+</html>
